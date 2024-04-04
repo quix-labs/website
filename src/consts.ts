e18d196015cb4052a22bd2cc056db220ddef7f4a
Alan Colant <alan.colant@outlook.fr>
2024-04-04 21:52:28 +0200

    try github

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,7 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const GITHUB_ORGANIZATION_NAME = 'quix-labs';
+export const GITHUB_EXCLUDED_REPOS = [".github"];
+export const GITHUB_API_TOKEN = "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";
